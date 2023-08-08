416ef1d2b00de8319d99a81f7fdf7898896ad4f4
Koundinya Vajjha <kody@imandra.ai>
2023-08-08 14:48:37 -0700

    feat: more testing of instance printers

--- bin/test_http_api_instance.ml
+++ bin/test_http_api_instance.ml
@@ -19,11 +19,20 @@ let () =
 
   let response =
     Log.debug (fun k -> k "Sending query to server...");
+    let* _ =
+      Default_api.eval
+        ~eval_request_src_t:
+          {
+            Imandra_http_api_client__Eval_request_src.src =
+              "let foo (x : int) = x + 1 ";
+            Imandra_http_api_client__Eval_request_src.syntax = Some `Iml;
+          }
+    in
     let* result =
       Default_api.instance_by_src
         ~instance_request_src_t:
           {
-            Instance_request_src.src = "fun (x : int) -> x > 4";
+            Instance_request_src.src = "fun (x : int) -> foo x > 4";
             Instance_request_src.syntax = Some `Iml;
             Instance_request_src.hints = None;
             Instance_request_src.instance_printer =
