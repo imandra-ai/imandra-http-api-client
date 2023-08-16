cbbcade2a690238b9022bb2adf91ddcba92bdf35
Dave Aitken <dave.aitken@gmail.com>
2023-08-16 11:35:42 +0100

    chore(nix): add ocamlformat

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.mkShell {
   buildInputs =
-    [ pkgs.czmq pkgs.gmp pkgs.opam pkgs.pkg-config pkgs.postgresql pkgs.zlib ]
+    [ pkgs.czmq pkgs.gmp pkgs.opam pkgs.pkg-config pkgs.postgresql pkgs.zlib pkgs.ocamlformat_0_21_0 ]
     ++ (if pkgs.stdenv.isDarwin then
       (with pkgs.darwin.apple_sdk.frameworks; [ CoreServices Foundation ])
     else
