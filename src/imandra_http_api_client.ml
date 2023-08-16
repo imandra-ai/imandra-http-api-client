635923f35ac4e1a16ceca3ea0d050f2c3660fd98
Dave Aitken <dave.aitken@gmail.com>
2023-08-16 10:51:47 +0100

    feat: fill out other endpoints

--- src/imandra_http_api_client.ml
+++ src/imandra_http_api_client.ml
@@ -48,86 +48,71 @@ let read (dec: 'a Decoders_yojson.Basic.Decode.decoder) (resp, body) : ('a Api.R
   in
   Lwt.return res
 
-let eval (c: Config.t) ~eval_req_src_t =
+let eval (c: Config.t) req =
   let open Lwt.Syntax in
   let uri = build_uri c "/eval/by-src" in
   let headers = default_headers c |> Cohttp.Header.of_list in
-  let body = make_body E.Request.eval_req_src eval_req_src_t in
+  let body = make_body E.Request.eval_req_src req in
   let* res = Cohttp_lwt_unix.Client.call `POST uri ~headers ~body in
   read D.Response.eval_result res
 
+let get_history (c: Config.t) =
+  let open Lwt.Syntax in
+  let uri = build_uri c "/history" in
+  let headers = default_headers c |> Cohttp.Header.of_list in
+  let* res = Cohttp_lwt_unix.Client.call `GET uri ~headers in
+  read Decoders_yojson.Basic.Decode.string res
 
-(* let get_history () = *)
-(*   let open Lwt.Infix in *)
-(*   let uri = build_uri "/history" in *)
-(*   let headers = Request.default_headers in *)
-(*   Cohttp_lwt_unix.Client.call `GET uri ~headers >>= fun (resp, body) -> *)
-(*   Request.read_string_body resp body *)
+let get_status (c: Config.t) =
+  let open Lwt.Syntax in
+  let uri = build_uri c "/status" in
+  let headers = default_headers c |> Cohttp.Header.of_list in
+  let* res = Cohttp_lwt_unix.Client.call `GET uri ~headers in
+  read Decoders_yojson.Basic.Decode.string res
 
-(* let get_status () = *)
-(*   let open Lwt.Infix in *)
-(*   let uri = build_uri "/status" in *)
-(*   let headers = Request.default_headers in *)
-(*   Cohttp_lwt_unix.Client.call `GET uri ~headers >>= fun (resp, body) -> *)
-(*   Request.read_string_body resp body *)
+let instance_by_src (c: Config.t) req =
+  let open Lwt.Syntax in
+  let uri = build_uri c "/instance/by-name" in
+  let headers = default_headers c |> Cohttp.Header.of_list in
+  let body = make_body E.Request.instance_req_name req in
+  let* res = Cohttp_lwt_unix.Client.call `POST uri ~headers ~body in
+  read D.Response.instance_result res
 
-(* let instance_by_name ~instance_request_name_t = *)
-(*   let open Lwt.Infix in *)
-(*   let uri = build_uri "/instance/by-name" in *)
-(*   let headers = Request.default_headers in *)
-(*   let body = *)
-(*     Request.write_as_json_body Instance_request_name.to_yojson *)
-(*       instance_request_name_t *)
-(*   in *)
-(*   Cohttp_lwt_unix.Client.call `POST uri ~headers ~body >>= fun (resp, body) -> *)
-(*   Request.read_json_body resp body *)
+let instance_by_src (c: Config.t) req =
+  let open Lwt.Syntax in
+  let uri = build_uri c "/instance/by-src" in
+  let headers = default_headers c |> Cohttp.Header.of_list in
+  let body = make_body E.Request.instance_req_src req in
+  let* res = Cohttp_lwt_unix.Client.call `POST uri ~headers ~body in
+  read D.Response.instance_result res
 
-(* let instance_by_src ~instance_request_src_t = *)
-(*   let open Lwt.Infix in *)
-(*   let uri = build_uri "/instance/by-src" in *)
-(*   let headers = Request.default_headers in *)
-(*   let body = *)
-(*     Request.write_as_json_body Instance_request_src.to_yojson *)
-(*       instance_request_src_t *)
-(*   in *)
-(*   Cohttp_lwt_unix.Client.call `POST uri ~headers ~body >>= fun (resp, body) -> *)
-(*   Request.read_json_body *)
-(*     (\* (JsonSupport.unwrap Instance_response.of_yojson) *\) *)
-(*     resp body *)
+let reset (c: Config.t) () =
+  let open Lwt.Syntax in
+  let uri = build_uri c "/reset" in
+  let headers = default_headers c |> Cohttp.Header.of_list in
+  let* res = Cohttp_lwt_unix.Client.call `POST uri ~headers in
+  read D.Response.reset_result res
 
-(* (\* TODO: fix in the server side to send reset info in the body? currently it returns "{}". *\) *)
-(* let reset () = *)
-(*   let open Lwt.Infix in *)
-(*   let uri = build_uri "/reset" in *)
-(*   let headers = Request.default_headers in *)
-(*   Cohttp_lwt_unix.Client.call `POST uri ~headers >>= fun (resp, body) -> *)
-(*   Request.read_string_body resp body *)
+let shutdown (c: Config.t) () =
+  let open Lwt.Syntax in
+  let uri = build_uri c "/shutdown" in
+  let headers = default_headers c |> Cohttp.Header.of_list in
+  let* res = Cohttp_lwt_unix.Client.call `POST uri ~headers in
+  read Decoders_yojson.Basic.Decode.string res
 
-(* let shutdown () = *)
-(*   let open Lwt.Infix in *)
-(*   let uri = build_uri "/shutdown" in *)
-(*   let headers = Request.default_headers in *)
-(*   Cohttp_lwt_unix.Client.call `POST uri ~headers >>= fun (resp, body) -> *)
-(*   (\* Request.read_json_body_as JsonSupport.to_string resp body *\) *)
-(*   Request.read_string_body resp body *)
 
-(* let verify_by_name ~verify_request_name_t = *)
-(*   let open Lwt.Infix in *)
-(*   let uri = build_uri "/verify/by-name" in *)
-(*   let headers = Request.default_headers in *)
-(*   let body = *)
-(*     Request.write_as_json_body Verify_request_name.to_yojson *)
-(*       verify_request_name_t *)
-(*   in *)
-(*   Cohttp_lwt_unix.Client.call `POST uri ~headers ~body >>= fun (resp, body) -> *)
-(*   Request.read_json_body resp body *)
+let verify_by_src (c: Config.t) req =
+  let open Lwt.Syntax in
+  let uri = build_uri c "/verify/by-name" in
+  let headers = default_headers c |> Cohttp.Header.of_list in
+  let body = make_body E.Request.verify_req_name req in
+  let* res = Cohttp_lwt_unix.Client.call `POST uri ~headers ~body in
+  read D.Response.verify_result res
 
-(* let verify_by_src ~verify_request_src_t = *)
-(*   let open Lwt.Infix in *)
-(*   let uri = build_uri "/verify/by-src" in *)
-(*   let headers = Request.default_headers in *)
-(*   let body = *)
-(*     Request.write_as_json_body Verify_request_src.to_yojson verify_request_src_t *)
-(*   in *)
-(*   Cohttp_lwt_unix.Client.call `POST uri ~headers ~body >>= fun (resp, body) -> *)
-(*   Request.read_json_body resp body *)
+let verify_by_src (c: Config.t) req =
+  let open Lwt.Syntax in
+  let uri = build_uri c "/verify/by-src" in
+  let headers = default_headers c |> Cohttp.Header.of_list in
+  let body = make_body E.Request.verify_req_src req in
+  let* res = Cohttp_lwt_unix.Client.call `POST uri ~headers ~body in
+  read D.Response.verify_result res
